363dc3b765e67577c1e041fe60ed745363c1c7c0
Aldo <aldo.wachyudi@gmail.com>
2018-02-19 11:46:33 +0700

    update description

--- app/src/main/java/com/aldoapps/opmsclass/hero/repository/HeroDatabase.kt
+++ app/src/main/java/com/aldoapps/opmsclass/hero/repository/HeroDatabase.kt
@@ -3,8 +3,6 @@ package com.aldoapps.opmsclass.hero.repository
 import android.os.SystemClock
 import com.aldoapps.opmsclass.R
 import com.aldoapps.opmsclass.util.RandomUtil
-import java.util.*
-import kotlin.collections.HashMap
 
 /**
  * Created by aldo on 04/01/18.
@@ -21,11 +19,11 @@ object HeroDatabase {
         HERO_ENTITY_LIST["Kamikaze"] = HeroEntity("Kamikaze", "Atomic Samurai", R.drawable.ic_kamikaze, 4, "World war II reference")
         HERO_ENTITY_LIST["Bofoi"] = HeroEntity("Bofoi", "Metal Knight", R.drawable.ic_bofoi, 6, "Eggman")
         HERO_ENTITY_LIST["King"] = HeroEntity("King", "Strongest Man", R.drawable.ic_king, 7, "Best Gundam pilot")
-        HERO_ENTITY_LIST["Watchdog Man"] = HeroEntity("Watchdog Man", "Watchdog Man", R.drawable.ic_watchdog_man, 12, "Eggman")
+        HERO_ENTITY_LIST["Watchdog Man"] = HeroEntity("Watchdog Man", "Watchdog Man", R.drawable.ic_watchdog_man, 12, "Half Dog Half Man")
         HERO_ENTITY_LIST["Genos"] = HeroEntity("Genos", "Demon Cyborg", R.drawable.ic_genos, 14, "History's strongest disciple")
-        HERO_ENTITY_LIST["Tanktop Master"] = HeroEntity("Tanktop Master", "Tanktop Master", R.drawable.ic_tanktop_master, 15, "TTM")
+        HERO_ENTITY_LIST["Tanktop Master"] = HeroEntity("Tanktop Master", "Tanktop Master", R.drawable.ic_tanktop_master, 15, "Cult lead")
         HERO_ENTITY_LIST["Bad"] = HeroEntity("Bad", "Metal Bat", R.drawable.ic_bad, 16, "If Hulk is a Japanese")
-        HERO_ENTITY_LIST["Puri-puri Prisoner"] = HeroEntity("Puri-puri Prisoner", "Puri-puri Prisoner", R.drawable.ic_puripuri_purisoner, 17, "Eggman")
+        HERO_ENTITY_LIST["Puri-puri Prisoner"] = HeroEntity("Puri-puri Prisoner", "Puri-puri Prisoner", R.drawable.ic_puripuri_purisoner, 17, "Serial Rapist")
         HERO_ENTITY_LIST["Sweet Mask"] = HeroEntity("Sweet Mask", "Sweet Mask", R.drawable.ic_sweet_mask, 18, "Ikemen trope")
     }
 
